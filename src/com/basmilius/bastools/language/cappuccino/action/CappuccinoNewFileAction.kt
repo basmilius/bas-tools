c42f23d3b5388199e47d7004cd32fa4d9fcb6b27
Bas Milius <basmilius@hotmail.com>
2018-01-18 17:35:00 +0100

    Started working on some new Cappuccino actions.

---
+++ src/com/basmilius/bastools/language/cappuccino/action/CappuccinoNewFileAction.kt
@@ -0,0 +1,20 @@
+package com.basmilius.bastools.language.cappuccino.action
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+/**
+ * Class CappuccinoNewFileAction
+ *
+ * @author Bas Milius <bas@mili.us>
+ * @package com.basmilius.bastools.language.cappuccino.action
+ * @since 1.3.1
+ */
+class CappuccinoNewFileAction: AnAction("Cappuccino File")
+{
+
+	override fun actionPerformed(aae: AnActionEvent)
+	{
+	}
+
+}
